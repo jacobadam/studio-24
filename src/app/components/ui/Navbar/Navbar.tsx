8862fd87e025c9b85de7319ddd7a8b7e539c8937
jacobadam <jacobanevitt@gmail.com>
2025-10-24 13:35:25 -0600

    update key for list

--- src/app/components/ui/Navbar/Navbar.tsx
+++ src/app/components/ui/Navbar/Navbar.tsx
@@ -38,10 +38,9 @@ export const NavBar = ({ links }: NavBarProps) => {
 
         <nav className="items-center justify-between hidden w-full md:flex md:w-auto">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-secondary-dark rounded-lg md:flex-row md:mt-0 md:border-0 gap-4">
-            {links?.map((link, i) => (
-              <li>
+            {links?.map((link) => (
+              <li key={link.label}>
                 <Link
-                  key={i}
                   href={link.href}
                   className="block py-2 px-3 text-primary rounded-sm md:bg-transparent md:p-0"
                   aria-current="page"
