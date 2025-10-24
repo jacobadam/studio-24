896dbca4626250732579144198bfa7a632554e76
jacobadam <jacobanevitt@gmail.com>
2025-10-24 10:01:26 -0600

    create reusable image component

---
+++ src/app/components/ui/HeroSection/HeroImage.tsx
@@ -0,0 +1,43 @@
+import { motion, useScroll, useTransform } from "motion/react";
+import React, { useRef } from "react";
+
+interface HeroImageProps {
+  src: string;
+}
+
+export const HeroImage = ({ src }: HeroImageProps) => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.85]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
+  const IMG_PADDING = 0;
+
+  return (
+    <main>
+      <motion.div
+        style={{
+          backgroundImage: `url(${src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: `calc(100vh - ${IMG_PADDING * 2}px)`,
+          top: IMG_PADDING,
+          scale,
+        }}
+        ref={targetRef}
+        className="sticky z-0 overflow-hidden"
+      >
+        <motion.div
+          className="absolute inset-0 bg-neutral-950/70"
+          style={{
+            opacity,
+          }}
+        />
+      </motion.div>
+    </main>
+  );
+};
