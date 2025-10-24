42288f0dbf47b39598423bc410abfc76667016cc
jacobadam <jacobanevitt@gmail.com>
2025-10-24 12:28:01 -0600

    update navbar to named import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "./components/ui/Navbar/Navbar";
+import { Navbar } from "./components/ui/Navbar/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
