bcdf28121c8a49b57cdd011533ffcf6a1bec78d6
jacobadam <jacobanevitt@gmail.com>
2025-10-29 09:55:30 -0600

    import Button and ButtonProps and map through heroButons prop to render Button component

--- src/app/components/ui/HeroSection/HeroSection.tsx
+++ src/app/components/ui/HeroSection/HeroSection.tsx
@@ -1,19 +1,30 @@
 import { HeroImage } from "./HeroImage";
 import { HeroText } from "./HeroText";
+import { Button, ButtonProps } from "../Button/Button";
 
-interface HeroProps {
+interface HeroSectionProps {
   src: string;
   subheading?: string;
   heading?: string;
   text?: string[];
+  heroButtons?: ButtonProps[];
 }
 
-export const HeroSection = ({ src, subheading, heading, text }: HeroProps) => {
+export const HeroSection = ({
+  src,
+  subheading,
+  heading,
+  text,
+  heroButtons,
+}: HeroSectionProps) => {
   return (
     <main>
       <div className="relative h-[150vh]">
         <HeroImage src={src} />
         <HeroText heading={heading} subheading={subheading} text={text} />
+        {heroButtons?.map((button, i) => (
+          <Button key={i} {...button} />
+        ))}
       </div>
     </main>
   );
