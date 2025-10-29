ca633518400ed8173afbf21105a4ca746c519668
jacobadam <jacobanevitt@gmail.com>
2025-10-29 09:54:45 -0600

    rename button props to heroButtons and pass in as array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 import { HeroSection } from "./components/ui/HeroSection/HeroSection";
 
 export default function HomePage() {
+  const handleClick = () => {
+    console.log("hit");
+  };
+
   return (
     <main className="bg-yellow-50 mt-16">
       <HeroSection
@@ -14,6 +18,18 @@ export default function HomePage() {
         src="/salon-tools.webp"
         heading="Lorem ipsum dolor sit amet."
         subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sagittis."
+        heroButtons={[
+          {
+            children: "Book Now",
+            variant: "secondary",
+            onClick: handleClick,
+          },
+          {
+            children: "Our Treatments",
+            variant: "primary",
+            href: "/treatments",
+          },
+        ]}
       ></HeroSection>
       <HeroSection
         src="/salon-chair.webp"
