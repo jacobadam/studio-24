8667ebc044dd04f865212be7299ca30c589a2d3d
jacobadam <jacobanevitt@gmail.com>
2026-01-13 10:47:57 -0500

    pass hero button component as hero text prop

--- src/app/components/ui/HeroSection/HeroSection.tsx
+++ src/app/components/ui/HeroSection/HeroSection.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-
 import { HeroImage } from "./HeroImage";
 import { HeroText } from "./HeroText";
-import { Button, ButtonProps } from "../Button/Button";
+import { ButtonProps } from "../Button/Button";
 
 interface HeroSectionProps {
   src: string;
@@ -25,12 +24,12 @@ export function HeroSection({
     <section>
       <div className="relative">
         <HeroImage src={src} />
-        <HeroText heading={heading} subheading={subheading} text={text} />
-        <div className="absolute bottom-20 left-1/2 flex -translate-x-1/2 gap-4">
-          {heroButtons?.map((button, i) => (
-            <Button key={i} {...button} />
-          ))}
-        </div>
+        <HeroText
+          heading={heading}
+          subheading={subheading}
+          text={text}
+          heroButtons={heroButtons}
+        />
       </div>
       <div className="bg-yellow-50">{content}</div>
     </section>
