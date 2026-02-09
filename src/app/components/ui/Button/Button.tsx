b8ed9fad5e9cfdb37d463883c13397d995d39338
jacobadam <jacobanevitt@gmail.com>
2026-02-09 12:24:01 -0600

    update button to add sizes prop

--- src/app/components/ui/Button/Button.tsx
+++ src/app/components/ui/Button/Button.tsx
@@ -3,9 +3,24 @@
 import React from "react";
 import Link from "next/link";
 
+const sizes = {
+  sm: "py-1.5 px-4 min-w-24 text-xs",
+  md: "py-2.5 px-3 min-w-32 text-sm",
+  lg: "py-2.5 px-5 min-w-40 text-sm",
+} as const;
+
+const variants = {
+  primary: "bg-primary text-secondary",
+  secondary: "bg-secondary text-primary",
+} as const;
+
+const baseStyles =
+  "inline-block rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-500";
+
 interface BaseProps {
   children: string;
   variant?: keyof typeof variants;
+  size?: keyof typeof sizes;
 }
 
 interface LinkButtonProps extends BaseProps {
@@ -20,29 +35,21 @@ interface ActionButtonProps extends BaseProps {
 
 export type ButtonProps = LinkButtonProps | ActionButtonProps;
 
-const styles =
-  "py-2.5 px-6 text-sm rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-500";
-const variants = {
-  primary: "bg-primary text-secondary",
-  secondary: "bg-secondary text-primary",
-} as const;
-
 export const Button = ({
   children,
   variant = "primary",
+  size = "md",
   onClick,
   href,
 }: ButtonProps) => {
+  const combinedStyles = `${baseStyles} ${variants[variant]} ${sizes[size]}`;
+
   return href ? (
-    <Link href={href} className={`${styles} ${variants[variant]}`}>
+    <Link href={href} className={combinedStyles}>
       {children}
     </Link>
   ) : (
-    <button
-      onClick={onClick}
-      type="button"
-      className={`${styles} ${variants[variant]}`}
-    >
+    <button onClick={onClick} type="button" className={combinedStyles}>
       {children}
     </button>
   );
